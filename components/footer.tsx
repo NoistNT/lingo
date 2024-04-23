e3afdca83fb4b37b58ccda0b0e365480122c6aa6
NoistNT <arielgnr23@gmail.com>
2024-04-23 19:22:42 -0300

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import { CardDescription, CardFooter } from './ui/card'
 
 export default function Footer() {
   return (
-    <CardFooter className="flex justify-center py-6 leading-[3rem] text-muted-foreground opacity-80">
+    <CardFooter className="hidden h-20 w-full items-center justify-center border-t-2 py-6 leading-[3rem] text-muted-foreground opacity-80 lg:flex">
       <CardDescription>
         Made with ❤️ by{' '}
         <Link
