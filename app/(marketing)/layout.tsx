4711018fbb430242188fdb2efc37b39df66a7745
NoistNT <arielgnr23@gmail.com>
2024-04-27 14:29:19 -0300

    Update marketing layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,7 +2,9 @@ import Footer from '@/app/(marketing)/footer'
 import Header from '@/app/(marketing)/header'
 import { ModeToggle } from '@/components/ui/mode-toggle'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({
+  children
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="fixed right-4 top-5 z-[150]">
