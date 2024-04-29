141abc76682abdc4f88c09e1733be922be8708c4
NoistNT <arielgnr23@gmail.com>
2024-04-29 11:53:06 -0300

    Update learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,17 @@
+import { redirect } from 'next/navigation'
+
 import FeedWrapper from '@/components/feed-wrapper'
 import StickyWrapper from '@/components/sticky-wrapper'
 import UserProgress from '@/components/user-progress'
+import { getUserProgress } from '@/db/queries'
 
 import Header from './header'
 
-export default function Learn() {
+export default async function LearnPage() {
+  const [userProgress] = await Promise.all([getUserProgress()])
+
+  if (!userProgress?.activeCourse) redirect('/courses')
+
   return (
     <div className="flex flex-row-reverse gap-12 px-6">
       <StickyWrapper>
