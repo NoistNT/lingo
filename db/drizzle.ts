725ac2602a3997d6e480597ed07cbda41730c1b6
NoistNT <arielgnr23@gmail.com>
2024-04-30 10:29:22 -0300

    Add drizzle schemas and queries

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,9 +1,11 @@
 import { neon } from '@neondatabase/serverless'
+import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
 
-import * as schema from './schema'
+import * as schema from '@/db/schema'
+
+config({ path: './.env.local' })
 
 const sql = neon(process.env.DB_URL!)
-const db = drizzle(sql, { schema })
 
-export default db
+export const db = drizzle(sql, { schema })
