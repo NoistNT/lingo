4fd02272889921f3f0055006a3555db7056fd826
NoistNT <arielgnr23@gmail.com>
2024-05-01 20:09:08 -0300

    Fetch data from db instead of hardcoded in learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,12 +3,16 @@ import { redirect } from 'next/navigation'
 import FeedWrapper from '@/components/feed-wrapper'
 import StickyWrapper from '@/components/sticky-wrapper'
 import UserProgress from '@/components/user-progress'
-import { getUserProgress } from '@/db/queries'
+import { getUnits, getUserProgress } from '@/db/queries'
 
 import Header from './header'
+import Unit from './unit'
 
 export default async function LearnPage() {
-  const [userProgress] = await Promise.all([getUserProgress()])
+  const [userProgress, units] = await Promise.all([
+    getUserProgress(),
+    getUnits()
+  ])
 
   if (!userProgress?.activeCourse) redirect('/courses')
 
@@ -16,17 +20,27 @@ export default async function LearnPage() {
     <div className="flex flex-row-reverse gap-12 px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{
-            title: 'Korean',
-            imgSrc: 'https://flagcdn.com/kr.svg'
-          }}
+          activeCourse={userProgress.activeCourse}
           hasActiveSubscription={false}
-          hearts={5}
-          points={100}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Korean" />
+        <Header title={userProgress.activeCourse.title} />
+        {units.map(({ id, title, order, lessons, description }) => (
+          <div key={id} className="mb-10">
+            <Unit
+              activeLesson={undefined}
+              activeLessonPercentage={0}
+              description={description}
+              id={id}
+              lessons={lessons}
+              order={order}
+              title={title}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   )
