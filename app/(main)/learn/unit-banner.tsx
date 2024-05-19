2ee4bba38d8076e19aff826dbf14cf4c922dad3d
NoistNT <arielgnr23@gmail.com>
2024-05-18 22:50:50 -0300

    Update unit banner component

--- app/(main)/learn/unit-banner.tsx
+++ app/(main)/learn/unit-banner.tsx
@@ -10,14 +10,17 @@ interface Props {
 
 export default function UnitBanner({ title, description }: Props) {
   return (
-    <div className="flex w-full items-center justify-between rounded-lg border border-neutral-200 bg-neutral-50 px-4 py-4 dark:border-neutral-800 dark:bg-neutral-900">
+    <div className="flex w-full items-center justify-between rounded-xl border border-neutral-200 bg-green-500 px-5 py-[0.98rem] text-white dark:border-neutral-800 dark:text-black">
       <div className="space-y-1.5">
-        <h3 className="text-xl font-bold">{title}</h3>
-        <p className="text-neutral-400">{description}</p>
+        <h3 className="text-2xl font-bold">{title}</h3>
+        <p>{description}</p>
       </div>
       <Link href="/lesson">
-        <Button className="hidden h-11 xl:flex " size="lg" variant="secondary">
-          <NotebookText className="mr-2.5 h-5 w-5" />
+        <Button
+          className="hidden h-11 border-2 border-b-4 px-6 active:border-b-2 xl:flex"
+          variant="secondary"
+        >
+          <NotebookText className="mr-2.5" />
           Continue
         </Button>
       </Link>
